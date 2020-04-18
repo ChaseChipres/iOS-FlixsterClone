020c30c05b51869f276d48b18194f8e9b2ae1d01
Chase Chipres <chasechipres@gmail.com>
2020-04-18 16:10:25 -0700

    Added collection view for superhero movies

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Chase Chipres on 4/17/20.
+//  Copyright © 2020 Chase Chipres. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
